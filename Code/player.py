da3db425e57b2be3c330df375135785524278d73
KyleG3369 <klg230001@utdallas.edu>
2026-04-22 02:24:38 -0500

    blah

--- Code/player.py
+++
@@ -1,7 +0,0 @@
-from settings import *
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
-        super().__init__(groups)
-        self.image = pygame.image.load(join('images', 'player', 'down', '0.png')).convert_alpha()
-        self.rect
