1a4ee6bd79a06f08947a40d6cf3c1b63ea708254
KyleG3369 <klg230001@utdallas.edu>
2026-05-05 02:47:03 -0500

    Collision

--- backup/main.py
+++ backup/main.py
@@ -15,13 +15,14 @@ class Game:
 
         # groups
         self.all_sprites = pygame.sprite.Group()
+        self.collision_sprites = pygame.sprite.Group()
 
         #sprites
-        self.Player = Player((400,300), self.all_sprites)
+        self.Player = Player((400,300), self.all_sprites, self.collision_sprites)
         for i in range(6):
             x, y = randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)
             w, h = randint(60, 100), randint(50, 100)
-            CollisionSprite((x, y), (w, h), self.all_sprites)
+            CollisionSprite((x, y), (w, h), (self.all_sprites, self.collision_sprites))
 
     def run(self):
         while self.running:
