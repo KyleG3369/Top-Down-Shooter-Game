d364e9ae03cf50ced79573641ab32aafbba1ab39
KyleG3369 <klg230001@utdallas.edu>
2026-05-05 02:21:48 -0500

    Setting the boundaries for collision.

--- backup/main.py
+++ backup/main.py
@@ -1,5 +1,8 @@
 from settings import *
 from player import Player
+from sprites import *
+
+from random import randint
 
 class Game:
     def __init__(self):
@@ -14,8 +17,11 @@ class Game:
         self.all_sprites = pygame.sprite.Group()
 
         #sprites
-
         self.Player = Player((400,300), self.all_sprites)
+        for i in range(6):
+            x, y = randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)
+            w, h = randint(60, 100), randint(50, 100)
+            CollisionSprite((x, y), (w, h), self.all_sprites)
 
     def run(self):
         while self.running:
