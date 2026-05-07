aa85df4e828ae2cf87001ac2ed8d08ed9cf42bd6
KyleG3369 <klg230001@utdallas.edu>
2026-05-07 02:44:45 -0500

    Setting up the map in game.

--- backup/main.py
+++ backup/main.py
@@ -18,12 +18,19 @@ class Game:
         self.all_sprites = pygame.sprite.Group()
         self.collision_sprites = pygame.sprite.Group()
 
+        self.setup()
+
         #sprites
         self.Player = Player((400,300), self.all_sprites, self.collision_sprites)
-        for i in range(6):
-            x, y = randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)
-            w, h = randint(60, 100), randint(50, 100)
-            CollisionSprite((x, y), (w, h), (self.all_sprites, self.collision_sprites))
+
+    def setup(self):
+        map = load_pygame(join('data', 'maps', 'world.tmx'))
+         
+        for x, y, image in map.get_layer_by_name('Ground').tiles():
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), image, self.all_sprites)
+        for obj in map.get_layer_by_name('Objects'):
+            CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
+        
 
     def run(self):
         while self.running:
