bfbc8eb8fe7eecf09b953ef9e61488941ece8ae9
KyleG3369 <klg230001@utdallas.edu>
2026-05-08 02:27:09 -0500

    Adding the gun to the player

--- backup/main.py
+++ backup/main.py
@@ -39,6 +39,7 @@ class Game:
         for obj in map.get_layer_by_name('Entities'):
             if obj.name == 'Player':
                 self.Player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
+                self.gun = Gun(self.Player, self.all_sprites)
 
     def run(self):
         while self.running:
--- backup/sprites.py
+++ backup/sprites.py
@@ -12,3 +12,24 @@ class CollisionSprite(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = surf
         self.rect = self.image.get_frect(topleft = pos)
+
+class Gun(pygame.sprite.Sprite):
+    def __init__(self, player, groups):
+        # player connection
+        self.player = player
+        self.distance = 140
+        self.player_direction = pygame.Vector2(0.25,0)
+
+        # sprite setup
+        super().__init__(groups)
+        self.gun_surf = pygame.image.load(join('images', 'gun', 'M92.png')).convert_alpha()
+        self.image = self.gun_surf
+        self.rect = self.image.get_frect(center = self.player.rect.center + self.player_direction * self.distance)
+    
+    def get_direction(self):
+        mouse_pos = pygame.Vector2(pygame.mouse.get_pos())
+        player_pos = pygame.Vector2(self.player.rect.center)
+
+    def update(self, _):
+        self.get_direction()
+        self.rect.center = self.player.rect.center + self.player_direction * self.distance
